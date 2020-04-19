0af3a9e329d607ce7abfad4bc8f7fa47ca19c050
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-19 13:29:15 -0600

    Create WindowsRickroll.js

---
+++ HID-Scripts/WindowsRickroll.js
@@ -0,0 +1,9 @@
+// Windows Rickroll
+// author: NightRang3r https://github.com/NightRang3r/P4wnP1-A.L.O.A.-Payloads/blob/master/HIDScripts/RickRoll%20Win.js
+// edited by: Coderdude112 on GitHub 
+// Will open Rickroll on youtube
+
+layout('us');
+press("GUI r");
+delay(500);
+type("iexplore -k https://www.youtube.com/watch?v=dQw4w9WgXcQ\n")

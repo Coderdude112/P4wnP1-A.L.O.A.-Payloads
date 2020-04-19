a2488da59ee21f48de5c928b09503db82250dc18
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-19 12:39:25 -0600

    Create FakeUpdate.js

---
+++ HID-Scripts/FakeUpdate.js
@@ -0,0 +1,18 @@
+// Fake update
+// author: Judge2020 https://judge2020.com
+// edited by: Coderdude112 on GitHub 
+// Pressing enter on the update screen will display a fake BSOD screen
+layout('us');		// US keyboard layout
+typingSpeed(0,0);	// TYPE FAST!
+press("GUI r");
+delay(500);
+// Select your OS option by un-commenting that line
+//type("iexplore -k http://fakeupdate.net/xp/\n") 		// Windows XP
+//type("iexplore -k http://fakeupdate.net/vista/\n") 	// Windows Vista
+//type("iexplore -k http://fakeupdate.net/win7/\n") 	// Windows 7
+//type("iexplore -k http://fakeupdate.net/win8/\n") 	// Windows 8
+//type("iexplore -k http://fakeupdate.net/apple/\n") 	// OSX update
+//type("iexplore -k http://fakeupdate.net/win10/\n") 	// Windows 10 Install
+//type("iexplore -k http://fakeupdate.net/win10ue/\n") 	// Windows 10 Update
+//type("iexplore -k http://fakeupdate.net/steam/\n") 	// Steam OS Update
+//type("iexplore -k http://fakeupdate.net/wnc/\n") 		// Wanna Cry Fake **NOTE: This is very obvious that it is a fake**

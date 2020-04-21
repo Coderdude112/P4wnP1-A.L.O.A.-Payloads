7f9ce1aca4da78c2a55b58627166ef8ab25c4459
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-21 09:23:21 -0600

    Update WindowsRickroll.js

--- HID-Scripts/WindowsRickroll.js
+++ HID-Scripts/WindowsRickroll.js
@@ -1,9 +1,9 @@
 // Windows Rickroll
-// author: NightRang3r https://github.com/NightRang3r/P4wnP1-A.L.O.A.-Payloads/blob/master/HIDScripts/RickRoll%20Win.js
-// edited by: Coderdude112 on GitHub 
-// Will open Rickroll on youtube
+// Author: NightRang3r https://github.com/NightRang3r/P4wnP1-A.L.O.A.-Payloads/blob/master/HIDScripts/RickRoll%20Win.js
+// Edited by: Coderdude112 on GitHub 
+// This attack will open Rickroll on youtube in fullscreen
 
 layout('us');
 press("GUI r");
 delay(500);
-type("iexplore -k https://www.youtube.com/watch?v=dQw4w9WgXcQ\n")
+type("iexplore -k https://www.youtube.com/watch?v=dQw4w9WgXcQ\n");

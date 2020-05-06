4e65bbb711f0536afc8166db4e4339c95aa76b53
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-05-05 20:29:15 -0600

    Create MouseJiggle.js

---
+++ HID-Scripts/Default-Scripts/MouseJiggle.js
@@ -0,0 +1,16 @@
+// Creds to Rogan Dawes, MouseJiggler idea was part of USaBUSe
+//
+// Moves the mouse slightly from time, to simulate user activity (supress screensaver/lockscreen)
+// Meant to run as background job
+// With extreme settings, it could be use to prank users
+
+scale = 2.0; //if not stepped, 127 is vali max
+sleeptime = 3000;
+stepped = true;
+while (true) {
+	x = (Math.random() * 2.0 - 1.0) * scale;
+	y = (Math.random() * 2.0 - 1.0) * scale;
+	if (stepped) moveStepped(x,y);
+	else move(x,y);
+	delay(sleeptime);
+}

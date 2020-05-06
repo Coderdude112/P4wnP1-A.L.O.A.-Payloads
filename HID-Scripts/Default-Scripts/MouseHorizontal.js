63663604854181fdbcc2f90dd91a75b4f79e8980
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-05-05 20:28:01 -0600

    Create MouseHorizontal.js

---
+++ HID-Scripts/Default-Scripts/MouseHorizontal.js
@@ -0,0 +1,14 @@
+// This attack comes with P4wnP1 A.L.O.A.
+// This attack moves the mouse left and right and is VERY frusturating
+
+step = Math.PI / 90.0;
+scale = 6.0
+alpha = 0.0
+horizontal = true;
+while(true) {
+	v = Math.sin(alpha) * scale;
+	alpha += step;
+	if (horizontal) moveStepped(v,0);
+	else moveStepped(0,v);
+	delay(10);
+}

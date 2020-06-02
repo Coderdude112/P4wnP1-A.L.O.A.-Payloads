9fee193b4c76f02d8253d80965e24e1aae96352d
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-06-02 11:16:12 -0600

    Update WindowsRickroll.js

--- HID-Scripts/WindowsRickroll.js
+++ HID-Scripts/WindowsRickroll.js
@@ -1,9 +1,35 @@
 // Windows Rickroll
 // Author: NightRang3r https://github.com/NightRang3r/P4wnP1-A.L.O.A.-Payloads/blob/master/HIDScripts/RickRoll%20Win.js
 // Edited by: Coderdude112 on GitHub 
-// This attack will open Rickroll on youtube in fullscreen
+// Will open Rickroll on youtube
 
-layout('us');
-press("GUI r");
-delay(500);
-type("iexplore -k https://www.youtube.com/watch?v=dQw4w9WgXcQ\n");
+layout('us');				// Set keyboard layout to US
+typingSpeed(0,0);			// Sets the typing speed to FAST
+var Ready = "False"			// Sets Ready to false beacuse we are not ready to begin the attack
+
+function Attack() { 		// Initalizes the function Attack
+  delay(2200);				// Delay to make sure that File Explorer doesent take focus
+  press("GUI r");			// Opens a run command
+  delay(500);
+
+  type("iexplore -k https://www.youtube.com/watch?v=dQw4w9WgXcQ\n");
+
+}
+
+function Initalized() { 	// Initalizes the function that checks if the device has been initalized by the host before running the attack
+  while (Ready = "False") {
+    press("SCROLL");
+    result = waitLED(SCROLL,10);
+    
+    if (result.SCROLL) { 	// If this is true then the device is initalized. The loop will break and the attack will start
+      Ready = "True";
+      break;   
+    } else {				// The device is not initalized so we will press scroll lock and retry
+        press("SCROLL");
+      }
+    
+  }
+}
+
+Initalized(); 				// Check if the host has initalized us
+Attack();					// Run the attack

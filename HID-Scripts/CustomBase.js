52b4eb8d0c700b1271cc8d6a0ab433e968bbdf1d
Calvin L <47407549+Coderdude112@users.noreply.github.com>
2020-06-02 11:14:37 -0600

    Update CustomBase.js

--- HID-Scripts/CustomBase.js
+++ HID-Scripts/CustomBase.js
@@ -3,21 +3,20 @@
 // Edited by: Coderdude112 on GitHub 
 // 
 
-layout('us');				  // Set keyboard layout to US
+layout('us');				// Set keyboard layout to US
 typingSpeed(0,0);			// Sets the typing speed to FAST
-var Ready = "False"		// Sets Ready to false beacuse we are not ready to begin the attack
+var Ready = "False"			// Sets Ready to false beacuse we are not ready to begin the attack
 
-function Attack() { 	// Initalizes the function Attack that acually exfiltrates the files
+function Attack() { 		// Initalizes the function Attack
   delay(2200);				// Delay to make sure that File Explorer doesent take focus
-  // Opens powershell
   press("GUI r");			// Opens a run command
   delay(500);
-  type("powershell\n");	 // Opens powershell
+  type("powershell\n");		// Opens powershell
   delay(500);
   
   type("powershell -windowstyle hidden {"); // Makes the window hidden for the duration of the attack
   
-  type("exit;}\n");  					            	// Adds the close curly bracket making the attack hidden and starts the attack 
+  type("exit;}\n");  						// Adds the close curly bracket making the attack hidden and starts the attack 
 }
 
 function Initalized() { 	// Initalizes the function that checks if the device has been initalized by the host before running the attack
@@ -28,7 +27,7 @@ function Initalized() { 	// Initalizes the function that checks if the device ha
     if (result.SCROLL) { 	// If this is true then the device is initalized. The loop will break and the attack will start
       Ready = "True";
       break;   
-    } else {				       // The device is not initalized so we will press scroll lock and retry
+    } else {				// The device is not initalized so we will press scroll lock and retry
         press("SCROLL");
       }
     
@@ -36,4 +35,4 @@ function Initalized() { 	// Initalizes the function that checks if the device ha
 }
 
 Initalized(); 				// Check if the host has initalized us
-Attack();					    // Run the attack
+Attack();					// Run the attack
